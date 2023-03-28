653d539e1e7576d4a4f221fca6f127cd2872a03f
Malik-Iskenderli <122275697+Malik-Iskenderli@users.noreply.github.com>
2023-03-28 16:35:26 +0400

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './css/App.css'
 
 function App() {
+
+  const [newItem, setNewItem]=useState("");
+  const [items,setItems]=useState([]);
+
+  function addItem(){
+    if(!newItem){
+      alert("bos buraxma");
+      return;
+    }
+
+
+    const item = {
+      id: Math.floor(Math.random()*1000),
+      value: newItem
+    }
+    setItems(oldItems => [...oldItems,item])
+    setNewItem("");
+  }
+  function deleteItem(id){
+    const newArray =items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+      style={{
+        color:'white',
+        paddingBottom:'2rem',
+        fontSize:'37px',
+
+      }}
+      >Todo list </h1>
+
+
+    <div style={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      gap:'0.7rem'
+    }}>
+      <input
+      className='inpt'
+      type="text"
+      placeholder='bir seyler yazin....'
+      value={newItem}
+      onChange={e=>setNewItem(e.target.value)}
+      />
+
+    <button className='btn1' onClick={()=>addItem()}>Add</button>
+    </div>
+    <ul className='ul'>
+      {items.map(item =>{
+        return(
+          <li className='li' key={item.id}>{item.value}<button className='btn2' onClick={()=>deleteItem(item.id)}> X</button></li>
+        )
+      })}
+    </ul>
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div style={{
+      width:'100%',
+      height:'100vh',
+      display:'inline-block',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'rgba(250, 170, 170, 0.815)'
+    }}>
     <App />
+  </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
